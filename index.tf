16c91b702939e01865d2fde5c58ef1148af9129f
Tommaso Girotto <tommaso.girotto91@gmail.com>
2025-02-09 12:57:11 +0100

    testing

--- index.tf
+++ index.tf
@@ -36,7 +36,8 @@ variable "netlify_access_token" {
 # then we need to use the local provider
 resource "null_resource" "netlify_deploy" {
   provisioner "local-exec" {
-    command = "chmod +x deploy.sh && source deploy.sh"
+    # command = "chmod +x deploy.sh && source deploy.sh"
+    command = "ls -alh"
     environment = {
       NETLIFY_ACCESS_TOKEN = var.netlify_access_token
     }
